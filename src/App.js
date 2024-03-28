3b85260eed5a1ddf641559b7ceb66abc6dc9ad25
heydns <98718194+heydns@users.noreply.github.com>
2024-03-28 11:50:47 +0000

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
+import './Login.css'
 import React from 'react';
 import AdminDashboard from './AdminDashboard/AdminDashboard';
+import LoginForm from "./LoginForm"
 
 function App() {
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <h1>Press button to view ticket</h1>
-        <AdminDashboard />
-      </header>
-    </div>
-  );
+    <header className="App-header">
+      <LoginForm />
+    </header>
+  </div>
+    
+  )
+
+
 }
 
 export default App;
